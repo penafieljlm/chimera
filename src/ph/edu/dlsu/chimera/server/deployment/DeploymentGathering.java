e0cf676a7fa22de4bab9089778bba01429db0665
penafieljlm <penafieljlm@gmail.com>
2013-10-31 22:52:23 +0800

    Multiple pipes for Gathering phase.

--- src/ph/edu/dlsu/chimera/server/deployment/DeploymentGathering.java
+++ src/ph/edu/dlsu/chimera/server/deployment/DeploymentGathering.java
@@ -86,23 +86,23 @@ public class DeploymentGathering extends Deployment {
         ConcurrentHashMap<SocketPair, Connection> stateTable = new ConcurrentHashMap<>();
 
         //daemons
-//        super.addComponent("stats", new ComponentStatisticsTable(assembly, assembly.getCriterias(), statsTableAtomic, statsTimeoutMs));
-//        super.addComponent("states", new ComponentStateTable(assembly, stateTable, stateTimeoutMs));
+        super.addComponent("stats", new ComponentStatisticsTable(assembly, assembly.getCriterias(), statsTableAtomic, statsTimeoutMs));
+        super.addComponent("states", new ComponentStateTable(assembly, stateTable, stateTimeoutMs));
 
         //inbound path
         //path 1 = sniffer  ->  injector
         //path 2 = sniffer  ->  stats   ->  states  ->  preprc  ->  dumper
-//        super.addComponent("ex.gather.sniff", new ComponentSniffer(assembly, this.externalGatherPcapPort, exGatherSniffOut, true));
-//        super.addComponent("ex.gather.stats", new ComponentStatisticsTracker(assembly, exGatherSniffOut, exGatherStatsOut, assembly.getCriterias(), statsTableAtomic));
-//        super.addComponent("ex.gather.states", new ComponentStateTracker(assembly, exGatherStatsOut, exGatherStateOut, stateTable));
-//        super.addComponent("ex.gather.preprc", new ComponentInstancePreprocessor(assembly, exGatherStateOut, exGatherPrePrcOut, assembly.getCriterias(), gatherAttacks));
-//        super.addComponent("ex.gather.dumper", new ComponentInstanceDumper(assembly, exGatherPrePrcOut, assembly.getCriterias(), trainingDumpFile));
+        super.addComponent("ex.gather.sniff", new ComponentSniffer(assembly, this.externalGatherPcapPort, exGatherSniffOut, true));
+        super.addComponent("ex.gather.stats", new ComponentStatisticsTracker(assembly, exGatherSniffOut, exGatherStatsOut, assembly.getCriterias(), statsTableAtomic));
+        super.addComponent("ex.gather.states", new ComponentStateTracker(assembly, exGatherStatsOut, exGatherStateOut, stateTable));
+        super.addComponent("ex.gather.preprc", new ComponentInstancePreprocessor(assembly, exGatherStateOut, exGatherPrePrcOut, assembly.getCriterias(), gatherAttacks));
+        super.addComponent("ex.gather.dumper", new ComponentInstanceDumper(assembly, exGatherPrePrcOut, assembly.getCriterias(), trainingDumpFile));
         
         super.addComponent("ex.bridge", new ComponentBridge(assembly, this.externalBridgePcapPort, this.internalBridgePcapPort));
 
         //outbound path
-//        super.addComponent("in.gather.sniff", new ComponentSniffer(assembly, this.internalGatherPcapPort, inGatherSniffOut, false));
-//        super.addComponent("in.gather.states", new ComponentStateTracker(assembly, inGatherSniffOut, null, stateTable));
+        super.addComponent("in.gather.sniff", new ComponentSniffer(assembly, this.internalGatherPcapPort, inGatherSniffOut, false));
+        super.addComponent("in.gather.states", new ComponentStateTracker(assembly, inGatherSniffOut, null, stateTable));
         
         super.addComponent("in.bridge", new ComponentBridge(assembly, this.internalBridgePcapPort, this.externalBridgePcapPort));
     }
