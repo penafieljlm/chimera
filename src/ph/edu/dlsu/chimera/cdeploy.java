b131c14e696322178689f23f3869923433fd0fab
penafieljlm <penafieljlm@gmail.com>
2013-11-27 13:48:10 +0800

    Decision Module prototype. Uses complicated system of multiple chains for enforcing rules.

--- src/ph/edu/dlsu/chimera/cdeploy.java
+++ src/ph/edu/dlsu/chimera/cdeploy.java
@@ -4,6 +4,13 @@
  */
 package ph.edu.dlsu.chimera;
 
+import org.jnetpcap.packet.JHeader;
+import org.jnetpcap.packet.structure.JField;
+import org.jnetpcap.protocol.lan.Ethernet;
+import org.jnetpcap.protocol.network.Ip4;
+import org.jnetpcap.protocol.tcpip.Tcp;
+import org.jnetpcap.protocol.tcpip.Udp;
+
 /**
  *
  * @author John Lawrence M. Penafiel <penafieljlm@gmail.com>
@@ -11,5 +18,9 @@ package ph.edu.dlsu.chimera;
 public class cdeploy {
 
     public static void main(String[] args) throws Exception {
+        JHeader p = new Udp();
+        for (JField f : p.getFields()) {
+            System.out.println(f.getName());
+        }
     }
 }
