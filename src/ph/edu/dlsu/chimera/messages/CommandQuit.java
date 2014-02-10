2becf57cda4139461dccdc54e23e8144bf12d964
Administrator <Administrator@WSTN-C05>
2014-02-10 18:32:39 +0800

    Improved monitors.

--- src/ph/edu/dlsu/chimera/messages/CommandQuit.java
+++ src/ph/edu/dlsu/chimera/messages/CommandQuit.java
@@ -1,6 +1,7 @@
 package ph.edu.dlsu.chimera.messages;
 
 import java.util.HashMap;
+import ph.edu.dlsu.chimera.Chimera;
 import ph.edu.dlsu.chimera.components.Component;
 import ph.edu.dlsu.chimera.components.ComponentActive;
 import ph.edu.dlsu.chimera.core.Session;
@@ -12,13 +13,7 @@ public class CommandQuit implements Command {
 
     @Override
     public Response handleCommand(Session session, HashMap<String, Component> assembly) throws Exception {
-        for (String c : assembly.keySet()) {
-            Component _c = assembly.get(c);
-            if (_c instanceof ComponentActive) {
-                ComponentActive _ca = (ComponentActive) _c;
-                _ca.kill();
-            }
-        }
+        Chimera.cquit(assembly);
         return new ResponseQuit();
     }
 }
